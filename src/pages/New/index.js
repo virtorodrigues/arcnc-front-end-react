5f80b2148ecafa8fd609f97dc98ed4b7d4f79c09
vitor <santos_vitao@hotmail.com>
2019-10-04 16:42:07 -0300

    ...

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -40,8 +40,8 @@ export default function New({ history }) {
                 id="thumbnail" 
                 style={{ backgroundImage: `url(${preview})` }}
                 className={thumbnail ? 'has-thumbnail' : ''}>
-                    <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
-                    <img src={ camera } alt="Select img" />
+                <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
+                <img src={ camera } alt="Select img" />
             </label>
 
            <label htmlFor="company">EMPRESA *</label>
@@ -49,24 +49,21 @@ export default function New({ history }) {
                 id="company"
                 placeholder="Sua empresa incrível"
                 value={company}
-                onChange={event => setCompany(event.target.value)}
-           />
+                onChange={event => setCompany(event.target.value)} />
 
             <label htmlFor="techs">TECNOLOGIAS * <span>(separadas por virgula)</span></label>
             <input 
                 id="techs"
                 placeholder="Quais tecnologias usam?"
                 value={techs}
-                onChange={event => setTechs(event.target.value)}
-            />
+                onChange={event => setTechs(event.target.value)} />
 
             <label htmlFor="price">VALOR DA DIÁRIA * <span>(em branco para GRATUITO)</span></label>
            <input 
                 id="price"
                 placeholder="Valor cobrado por dia"
                 value={price}
-                onChange={event => setPrice(event.target.value)}
-            />
+                onChange={event => setPrice(event.target.value)} />
 
             <button type="submit" className="btn" >Cadastrar</button>
        </form>
